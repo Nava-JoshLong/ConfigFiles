8a976ad483d84f3a48b3c61118848ee821cc2933
Nava-JoshLong <65624843+Nava-JoshLong@users.noreply.github.com>
2020-11-23 07:43:19 -0800

    Create installation.sh
    This is the script that will need to be ran to install CWAgent

---
+++ CWAgent/installation.sh
@@ -0,0 +1,9 @@
+yum update -y
+yum install -y rpm
+rpm -ivh https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+yum install -y collectd
+cd /opt/aws/amazon-cloudwatch-agent/bin/
+curl "https://raw.githubusercontent.com/Nava-JoshLong/ConfigFiles/main/CWAgent/config.json" -o "config.json"
+./amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:config.json -s
+./amazon-cloudwatch-agent-ctl -a stop
+./amazon-cloudwatch-agent-ctl -a start
